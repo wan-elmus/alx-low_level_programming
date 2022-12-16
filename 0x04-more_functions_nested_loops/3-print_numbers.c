51f54dac91f766d538a811a80130831568399cd3
wan-elmus <elmuswangudi@gmail.com>
2022-12-15 20:12:46 -0800

    modified task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 
 void print_numbers(void)
 {
-	char c;
+        char c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+        for (c = '0'; c <= '9'; c++)
+        {
+        _putchar(c);
+        }
+        _putchar('\n');
 }
