96bed70e991618875b4a5f2ae7dbffe3631024a1
wan-elmus <elmuswangudi@gmail.com>
2022-12-15 20:21:53 -0800

    created multiple files

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+*print_numbers - print 0123456789
+* wan-elmus
+*Return: void
+*/
+
 void print_numbers(void)
 {
-        char c;
+	char c;
 
-        for (c = '0'; c <= '9'; c++)
-        {
-        _putchar(c);
-        }
-        _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
