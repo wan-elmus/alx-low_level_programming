7f49212ace16bbdc48e644cd3aab1cc8ec4a28bc
wan-elmus <elmuswangudi@gmail.com>
2022-12-20 19:51:36 +0300

    function that reverses a string

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-* _isdigit - write a function that check for a digit (0 through 9).
-* @c: char to check
-* Return:  0 or 1
-**/
-int _isdigit(int c)
+ * _isdigit - write a function that check for a digit (0 through 9).
+ * @c: char to check
+ * Return:  0 or 1
+ **/
+int main()
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+	int _isdigit(int c)
+	{
+		if (c >= '0' && c <= '9')
+			return (1);
+		else
+			return (0);
+	}
 }
