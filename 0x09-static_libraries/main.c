3697d5e58f4096445ca41fa28e83021f42c99916
wan-elmus <elmuswangudi@gmail.com>
2023-01-09 23:56:26 +0300

    changes

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	_puts("\"Anyone can break into tech, it takes coomitment and grit to maintain\"\n\t- sensei");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
